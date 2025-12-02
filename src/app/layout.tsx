6f0b14e827c34ebd50d79047c05cc0fdd2963add
OnyiKaroli <karolionyango@gmail.com>
2025-12-03 01:09:29 +0300

    feat: Add Vercel Analytics dependency and integrate it into the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   variable: "--font-inter",
@@ -39,6 +40,7 @@ export default function RootLayout({
           className={`${inter.variable} antialiased`}
         >
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
